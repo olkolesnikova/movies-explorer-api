ee5f074358b69a8633a40d3c4891fdb7c0a593ff
olkolesnikova90 <olkolesnikova90@yandex.ru>
2023-09-28 02:10:13 +0500

    2

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('Connected to db');
+});
 const app = express();
-
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
