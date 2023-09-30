c76821744e4082b7ce575394cff0ca419ff010ef
olkolesnikova90 <olkolesnikova90@yandex.ru>
2023-09-30 17:41:49 +0500

    3009

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,14 @@ const {
   login, createUser, getUserInfo, updateUserInfo,
 } = require('../controllers/users');
 const {
-  loginValidation, registrationValidation, userInfoValidation, updateUserInfoValidation,
+  loginValidation, registrationValidation, updateUserInfoValidation,
 } = require('../validation/user-joi-validation');
 const auth = require('../middlewares/auth');
 
 userRouter.post('/signin', loginValidation, login);
 userRouter.post('/signup', registrationValidation, createUser);
 userRouter.use(auth);
-userRouter.get('/users/me', userInfoValidation, getUserInfo);
+userRouter.get('/users/me', getUserInfo);
 userRouter.patch('/users/me', updateUserInfoValidation, updateUserInfo);
 
 module.exports = userRouter;
